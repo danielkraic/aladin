cd8cad5af7d742b7d2ec68bfd3dd6e753d3409c9
Daniel Kraic <daniel.kraic@eset.com>
2018-11-24 23:35:48 +0100

    Add support for other meteogram types

--- aladin.js
+++ aladin.js
@@ -1,45 +1,42 @@
-function get_aladin_url(city_id, date, time) {
-    return 'http://www.shmu.sk/data/datanwp/v2/meteogram/al-meteogram_' + city_id + '-' + date + '-' + time + '-nwp-.png';
+function get_image(meteogram_type, city_id, date, time) {
+    return {
+        src: `http://www.shmu.sk/data/datanwp/v2/${meteogram_type}_${city_id}-${date}-${time}-nwp-.png`
+    };
 }
 
-function get_last_aladins(city_id) {
-    var d = new Date()
-    var today_str = "" +  d.getFullYear() + ("0"+(d.getMonth()+1)).slice(-2) + ("0" + d.getDate()).slice(-2);
+function get_images_for_day(city_id, meteogram_type, date) {
+    return ['1800', '1200', '0600', '0000'].map(function (time) {
+        return get_image(meteogram_type, city_id, date, time);
+    });
+}
 
-    d.setDate(d.getDate() - 1)
-    var yesterday_str = "" +  d.getFullYear() + ("0"+(d.getMonth()+1)).slice(-2) + ("0" + d.getDate()).slice(-2);
-    
-    return [
-        {src: get_aladin_url(city_id, today_str, '1800')},
-        {src: get_aladin_url(city_id, today_str, '1200')},
-        {src: get_aladin_url(city_id, today_str, '0600')},
-        {src: get_aladin_url(city_id, today_str, '0000')},
+function get_images(city_id, meteogram_type) {
+    var d = new Date();
+    var today = "" + d.getFullYear() + ("0" + (d.getMonth() + 1)).slice(-2) + ("0" + d.getDate()).slice(-2);
 
-        {src: get_aladin_url(city_id, yesterday_str, '1800')},
-        {src: get_aladin_url(city_id, yesterday_str, '1200')},
-        {src: get_aladin_url(city_id, yesterday_str, '0600')},
-        {src: get_aladin_url(city_id, yesterday_str, '0000')}
-    ]
+    d.setDate(d.getDate() - 1);
+    var yesterday = "" + d.getFullYear() + ("0" + (d.getMonth() + 1)).slice(-2) + ("0" + d.getDate()).slice(-2);
+
+    images_today = get_images_for_day(city_id, meteogram_type, today);
+    images_yesterday = get_images_for_day(city_id, meteogram_type, yesterday);
+
+    return images_today.concat(images_yesterday);
 }
 
 new Vue({
     el: '#app',
     data: {
-        selected_city: '32737',
-        aladin_img_srcs: [],
-        options: [
-            { text: 'Bratislava', value: '32737' },
-            { text: 'Trnava', value: '31360' },
-            { text: 'Cadca', value: '31438' }
+        selected_type: 'meteogram/al-meteogram',
+        selected_city: '31360',
+        available_cities: [
+            { id: '32737', name: 'Bratislava' },
+            { id: '31360', name: 'Trnava' },
+            { id: '31438', name: 'Cadca' }
         ]
     },
-    created: function() {
-        this.update_aladin()
-    },
-    methods: {  
-        update_aladin: function() {
-            this.aladin_img_srcs =  get_last_aladins(this.selected_city)
-            console.log('srcs: ', this.aladin_img_srcs)
+    computed: {
+        imgs: function () {
+            return get_images(this.selected_city, this.selected_type);
         }
     }
-})
+});
